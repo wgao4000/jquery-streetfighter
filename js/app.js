83287b8902e95ebe3720eff8754a629821086045
wgao4000 <wgao4000@gmail.com>
2015-09-09 13:31:42 -0400

    making small changes such as adding my name, etc

--- js/app.js
+++ js/app.js
@@ -1,71 +1,76 @@
+/*  Author: Wei Gao
+    Date: 05/2014
+*/
 $(document).ready(function(){
-   var mouseenter,mousedown;
-   $('.ryu').mouseenter(function(){
-     $('.ryu-still').hide();
-     $('.ryu-ready').show();
-     mouseenter=1;
-   })
+    var mouseenter,mousedown;
+    $('.ryu').mouseenter(function(){
+      $('.ryu-still').hide();
+      $('.ryu-ready').show();
+      mouseenter=1;
+    })
    
-   .mouseleave(function(){
-     $('.ryu-ready').hide();
-     $('.ryu-still').show();
-     mouseenter=0;
-   })
-   .mousedown(function(){
-     mousedown=1;
-     playHadouken();
-     anima();
-   })
+    .mouseleave(function(){
+      $('.ryu-ready').hide();
+      $('.ryu-still').show();
+      mouseenter=0;
+    })
    
-   .mouseup(function(){
+    .mousedown(function(){
+      mousedown=1;
+      playHadouken();
+      anima();
+    })
+   
+    .mouseup(function(){
       mousedown=0;
       $('.ryu-throwing').hide();
       $('.ryu-ready').show();
-      });
+    });
   
    $(this).keydown(function(event){
       if(event.which==88){
-       $('.ryu-still').hide();
-       $('.ryu-ready').hide();
-       $('.ryu-throwing').hide();
-       $('.ryu-cool').show();
-       $('.hadouken').hide();
+        $('.ryu-still').hide();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+        $('.ryu-cool').show();
+        $('.hadouken').hide();
       }
     })
    
     .keyup(function(event){
       if(event.which==88){
-         $('.ryu-cool').hide();
-         if(mouseenter==0)
+        $('.ryu-cool').hide();
+        if(mouseenter==0)
           $('.ryu-still').show();
-         else if(mouseenter==1){
-            if(mousedown==0)
-             $('.ryu-ready').show();
-            else if(mousedown==1){
+        else if(mouseenter==1){
+          if(mousedown==0)
+            $('.ryu-ready').show();
+          else if(mousedown==1){
              $('.ryu-cool').hide();
              playHadouken();
              anima();
-            }   
-         }
-       }
+          }   
+        }
+      }
    });
  });
- function playHadouken(){
-    $('#hadouken-sound')[0].volume=0.5;
-    $('#hadouken-sound')[0].load();
-      $('#hadouken-sound')[0].play();
- }
-
- function anima(){
-     $('.ryu-ready').hide();
-     $('.ryu-throwing').finish().show();
-     $('.hadouken').show()
-    .animate(
-      {'left':'520px'},
-       500,
-       function(){
-           $(this).hide();
-           $(this).css('left','500px');
-       }
-      )
+//playing hadouken 
+function playHadouken(){
+  $('#hadouken-sound')[0].volume=0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+// animation
+function anima(){
+  $('.ryu-ready').hide();
+  $('.ryu-throwing').finish().show();
+  $('.hadouken').show()
+  .animate(
+    {'left':'520px'},
+    500,
+    function(){
+      $(this).hide();
+      $(this).css('left','500px');
+    }
+  )
 }    
